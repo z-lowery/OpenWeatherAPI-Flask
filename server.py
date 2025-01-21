c0d63ab0ccbbd33b9fc1fdc587fe0bacd0a5374c
z-lowery <z.lowery@outlook.com>
2025-01-20 20:48:10 -0600

    Added additional comments for personal learning and comprehension

--- server.py
+++ server.py
@@ -1,27 +1,27 @@
-from flask import Flask, render_template, request 
-from weather import get_current_weather # import function from weather.py
-from waitress import serve # helps serve our application on the web
+from flask import Flask, render_template, request # Imports necessary components from Flask, function to render HTML templates, and handle incoming data
+from weather import get_current_weather # Import function from weather.py
+from waitress import serve # Helps serve our application on the web by importing the 'serve' function. Manages multiple simultaneous HTTP request more efficiently than the Flask development server, 
 
-app = Flask(__name__) # creates a Flask app
+app = Flask(__name__) # Creates a Flask app
 
-@app.route('/') # accesses the root URL (homepage)
+@app.route('/') # Accesses the root URL (homepage)
 @app.route('/index')
 
 def index():
-    return render_template('index.html') # renders the index.html template
+    return render_template('index.html') # Renders the index.html template
 
-@app.route('/weather') # accesses the /weather URL
+@app.route('/weather') # Accesses the /weather URL
 def get_weather():
-    city = request.args.get('city') # get the city from the URL
-    weather_data = get_current_weather(city) # get the weather data for the city
-    return render_template( # render the weather.html template with the weather data
+    city = request.args.get('city') # Get the city from the URL
+    weather_data = get_current_weather(city) # Get the weather data for the city
+    return render_template( # Render the weather.html template with the weather data
         'weather.html', 
-        # sets various variables that weather.html will use 
+        # Sets various variables that weather.html will use 
         title = weather_data["name"], 
-        status = weather_data["weather"][0]["description"].capitalize(), # because "weather" is a list, we need to access the first element
+        status = weather_data["weather"][0]["description"].capitalize(), # Because "weather" is a list, we need to access the first element
         temp=f"{weather_data['main']['temp']:.1f}", # .1f rounds the temperature to one decimal place
         feels_like=f"{weather_data['main']['feels_like']:.1f}"
     )
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000) # runs the app locally on port 8000
+    serve(app, host="0.0.0.0", port=8000) # Runs the app locally on port 8000
